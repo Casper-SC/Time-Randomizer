f4f3735628ceece95cb4a7a15f10a0942fea88cc
Casper-SC <caspersc2@gmail.com>
2016-04-25 00:48:48 +0300

    Добавил иконку

--- TimeRandomizer/Properties/AssemblyInfo.cs
+++ TimeRandomizer/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TimeRandomizer")]
+[assembly: AssemblyTitle("Time Randomizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("a3f8bf44-7fe0-4dee-b53b-3ed0e1aa1143")]
+
